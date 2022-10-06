fa89d182bce32695aa49de3bd934792b1c7e3a50
melvincwng <flaw_ng@hotmail.com>
2022-10-06 22:27:03 +0800

    Create basic homepage implementation

---
+++ community-website/pages/community-guidelines.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const CommunityGuidelines: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      Community Guidelines Placeholder
+      <br />
+      <a href="/">Go Back</a>
+    </div>
+  );
+};
+
+export default CommunityGuidelines;
