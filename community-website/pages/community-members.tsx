2a55e12e7bd944ced047e24e66457442e88228e6
melvincwng <melvin.ng@outlook.sg>
2022-10-06 23:27:02 +0800

    Fix build errors

--- community-website/pages/community-members.tsx
+++ community-website/pages/community-members.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 const CommunityMembers: NextPage = () => {
   return (
