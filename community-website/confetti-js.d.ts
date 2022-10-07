4574ab7ca053ad83021a2bcb33054d61aa2247b9
melvincwng <melvin.ng@outlook.sg>
2022-10-08 00:30:36 +0800

    Add confetti-js.d.ts to fix build issue

---
+++ community-website/confetti-js.d.ts
@@ -0,0 +1,2 @@
+// Fix for 'Could not find a declaration file for confetti-js module' - https://bobbyhadz.com/blog/typescript-could-not-find-a-declaration-file-for-module
+declare module 'confetti-js';
