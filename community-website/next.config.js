65e93c592fa9155a469dadb2c669ecd1c35e9791
melvincwng <melvin.ng@outlook.sg>
2022-10-08 10:50:42 +0800

    Add basic navbar from tailwind-css

--- community-website/next.config.js
+++ community-website/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["images.unsplash.com", "tailwindui.com", 'localhost:3000'],
+  },
 }
 
 module.exports = nextConfig
