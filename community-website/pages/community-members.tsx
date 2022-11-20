8f98d57cd2241e2c49a90b07fb33c0c640f6123f
melvincwng <melvin.ng@outlook.sg>
2022-11-21 01:26:19 +0800

    Add Bennykillua

--- community-website/pages/community-members.tsx
+++ community-website/pages/community-members.tsx
@@ -2004,6 +2004,25 @@ const CommunityMembers: NextPage = () => {
                     </sub>
                   </a>
                 </td>
+                <td align="center">
+                  <a
+                    href="https://github.com/Bennykillua"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src="https://avatars.githubusercontent.com/u/67695793?v=4"
+                      width="85"
+                      height="85"
+                      className={styles.profileImage}
+                      alt="Bennykillua"
+                    />
+                    <br />
+                    <sub>
+                      <b>Bennykillua</b>
+                    </sub>
+                  </a>
+                </td>
               </tr>
             </tbody>
           </table>
