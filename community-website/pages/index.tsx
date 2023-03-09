7fcbe20f2949bd346954ac2d2359545581927461
melvincwng <melvin.ng@outlook.sg>
2023-03-09 19:24:46 +0800

    Update member count

--- community-website/pages/index.tsx
+++ community-website/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
       strings: [
         "Get started with Your First Open Source Project!",
         "Submit your first open source contribution now!",
-        "Join our awesome community of 130+ members!",
+        "Join our awesome community of 135+ members!",
       ],
       typeSpeed: 75,
       backSpeed: 75,
@@ -111,7 +111,7 @@ const Home: NextPage = () => {
               rel="noopener noreferrer"
             >
               <h2>Find us on GitHub &rarr;</h2>
-              <p>Join our wonderful & growing community of 130+ people 💖</p>
+              <p>Join our wonderful & growing community of 135+ people 💖</p>
             </a>
           </Link>
         </div>
