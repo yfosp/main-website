fb3d666f62127500460c33dedec7413c78a31c54
melvincwng <melvin.ng@outlook.sg>
2023-03-28 17:01:07 +0800

    Add members - 28/03/2023

--- community-website/pages/index.tsx
+++ community-website/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
       strings: [
         "Get started with Your First Open Source Project!",
         "Submit your first open source contribution now!",
-        "Join our awesome community of 138+ members!",
+        "Join our awesome community of 139+ members!",
       ],
       typeSpeed: 75,
       backSpeed: 75,
@@ -111,7 +111,7 @@ const Home: NextPage = () => {
               rel="noopener noreferrer"
             >
               <h2>Find us on GitHub &rarr;</h2>
-              <p>Join our wonderful & growing community of 138+ people 💖</p>
+              <p>Join our wonderful & growing community of 139+ people 💖</p>
             </a>
           </Link>
         </div>
