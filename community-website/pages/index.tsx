0946902ec0a3e69722bfdd730c9cc2b35095d4d5
dollarboysushil <thesushilpoudel@gmail.com>
2023-10-03 18:05:51 +0545

    Fixed member count from 170+ to 178+ in community website

--- community-website/pages/index.tsx
+++ community-website/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
       strings: [
         "Get started with Your First Open Source Project!",
         "Submit your first open source contribution now!",
-        "Join our awesome community of 170+ members!",
+        "Join our awesome community of 178+ members!",
       ],
       typeSpeed: 75,
       backSpeed: 75,
@@ -111,7 +111,7 @@ const Home: NextPage = () => {
               rel="noopener noreferrer"
             >
               <h2>Find us on GitHub &rarr;</h2>
-              <p>Join our wonderful & growing community of 170+ people 💖</p>
+              <p>Join our wonderful & growing community of 178+ people 💖</p>
             </a>
           </Link>
         </div>
